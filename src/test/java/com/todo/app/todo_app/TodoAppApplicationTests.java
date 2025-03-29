f6ca6a0dbdab8fb5177d5dabc83f79f137b9ef7d
abhinav.choudhary <choudhary.abhinav1503@gmail.com>
2025-03-30 02:21:57 +0530

    update

--- src/test/java/com/todo/app/todo_app/TodoAppApplicationTests.java
+++ src/test/java/com/todo/app/todo_app/TodoAppApplicationTests.java
@@ -3,11 +3,14 @@ package com.todo.app.todo_app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class TodoAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat("a").isEqualTo("a");
 	}
 
 }
